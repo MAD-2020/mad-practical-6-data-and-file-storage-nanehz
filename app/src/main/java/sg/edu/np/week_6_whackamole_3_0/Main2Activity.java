0abade02a5664dd5e6fec04400d0965715424390
Zhen An Choa <s10198063@connect.np.edu.sg>
2020-06-16 21:52:25 +0800

    done with creation of new user

--- app/src/main/java/sg/edu/np/week_6_whackamole_3_0/Main2Activity.java
+++ app/src/main/java/sg/edu/np/week_6_whackamole_3_0/Main2Activity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.service.autofill.AutofillService;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -40,46 +41,32 @@ public class Main2Activity extends AppCompatActivity {
         username_input = findViewById(R.id.username_input);
         password_input = findViewById(R.id.password_input);
 
-        final MyDBHandler dbHandler = new MyDBHandler(this,null,null,1);
+        final MyDBHandler myDatabase = new MyDBHandler(this,null,null,1);
 
+        createBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserData userData = myDatabase.findUser(username_input.getText().toString().trim());
 
-//        createBTN.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//
-//                UserData userData = new UserData();
-//                userData.setMyUserName(username_input.getText().toString());
-//                userData.setMyPassword(password_input.getText().toString());
-//
-//                if (dbHandler.findUser(username_input.getText().toString())  == userData)
-//                {
-//                    Toast.makeText(getApplicationContext(),"User already Exists. Please try again.",Toast.LENGTH_SHORT).show();
-//                    Log.v(TAG, FILENAME + ": User already exist during new user creation!");
-//                }
-//                else
-//                {
-//
-//                    UserData dbUserData = new UserData();
-//                    dbUserData.setLevels(new ArrayList<Integer>());
-//                    dbUserData.setMyUserName(username_input.getText().toString());
-//                    dbUserData.setMyPassword(password_input.getText().toString());
-//                    dbHandler.addUser(dbUserData);
-//                    username_input.setText("");
-//                    password_input.setText("");
-//                    Log.v(TAG, FILENAME + ": New user created successfully!");
-//                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
-////                    dbHandler.addUser(userData);
-////                    username_input.setText("");
-////                    password_input.setText("");
-////                    Log.v(TAG, FILENAME + ": New user created successfully!");
-////                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
-//                }
-//
-//            }
-//        });
+                if (userData != null)
+                {
+                    Log.v(TAG, FILENAME + ": User already exist during new user creation!");
+                    Toast.makeText(getApplicationContext(), "User already exist during new user creation!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    UserData newUser = new UserData(username_input.getText().toString().trim(),password_input.getText().toString().trim(),new ArrayList<Integer>(), new ArrayList<Integer>());
+                    myDatabase.addUser(newUser);
+                    Log.v(TAG, FILENAME + ": New user created successfully!");
+                    Toast.makeText(getApplicationContext(), "New user created successfully!", Toast.LENGTH_SHORT).show();
+                    username_input.setText("");
+                    password_input.setText("");
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
 
+                }
 
+            }
+        });
 
         /* Hint:
             This prepares the create and cancel account buttons and interacts with the database to determine
